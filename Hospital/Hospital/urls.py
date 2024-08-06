c015c5901b918e826ed9ac949b51af9cf0be5d98
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-08-06 11:54:44 +0300

    Rename 'user' directory to 'users' for consistency.

--- Hospital/Hospital/urls.py
+++ Hospital/Hospital/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('users/', include('user.urls')),
+    path('users/', include('users.urls')),
 ]
