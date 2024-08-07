80fb4d00625323f3a546758349f987cb9a6de3e0
Sarah <monifsarah@gmail.com>
2024-08-07 13:19:00 +0300

    users can make reservations

--- Hospital/reservations/urls.py
+++ Hospital/reservations/urls.py
@@ -6,7 +6,7 @@ app_name = "reservations"
 
 urlpatterns = [
     path('select_clinic/', views.select_clinic, name='select_clinic'),
-    path('select_doctor/', views.select_doctor, name='select_doctor'),
+    path('select_doctor/<int:clinic_id>', views.select_doctor, name='select_doctor'),
     path("add/",views.make_reservation,name="make_reservation"),
 
 ]
--- Hospital/reservations/views.py
+++ Hospital/reservations/views.py
@@ -3,30 +3,43 @@ from django.http import HttpRequest, HttpResponse
 from .forms import ReservationForm
 from django.contrib import messages
 
-from .models import Clinic, Doctor
+from .models import Clinic, Doctor,Reservation,Profile
 
-def select_clinic(request):
+def select_clinic(request:HttpRequest):
     clinics = Clinic.objects.all()
     return render(request, 'reservations/select_clinic.html', {'clinics': clinics})
 
-def select_doctor(request):
-    clinic_id = request.POST.get('clinic')
-    clinic = Clinic.objects.get(id=clinic_id)
+def select_doctor(request:HttpRequest,clinic_id):
+    clinic = Clinic.objects.get(pk=clinic_id)
     doctors = Doctor.objects.filter(clinic=clinic)
     return render(request, 'reservations/select_details.html', {'clinic': clinic, 'doctors': doctors})
 
-def make_reservation(request: HttpRequest):
-    if request.method == "POST":
-        reservationForm = ReservationForm(request.POST, request.FILES)
-        if reservationForm.is_valid():
-            reservationForm.save()
-            messages.success(request, 'Doctor added successfully!', 'alert-success')
-            return redirect("main:home")
-        else:
-            for field, errors in reservationForm.errors.items():
-                for error in errors:
-                    messages.error(request, f"{field}: {error}", 'alert-danger')
-    else:
-        reservationForm = ReservationForm()
 
-    return render(request, "reservations/make_reservation.html", {'form': reservationForm})
+def make_reservation(request:HttpRequest):
+    if not request.user.is_authenticated:
+        messages.error(request, "You need to be logged in to make a reservation", "alert-danger")
+        return redirect("accounts:sign_in")
+
+    if request.method == "POST":
+        user_profile = Profile.objects.get(user=request.user)
+
+       
+        clinic_id = request.POST.get("clinic")
+        clinic_instance = Clinic.objects.get(id=clinic_id)
+
+        doctor_id=request.POST.get("doctor")
+        doctor_instance = Doctor.objects.get(id=doctor_id)
+
+        new_reservation = Reservation(
+            user=user_profile, 
+            time_slot=request.POST.get("time_slot"),
+            date=request.POST.get("date"),  
+            clinic=clinic_instance,  
+            doctor=doctor_instance,  
+        )
+        new_reservation.save()
+
+        messages.success(request, "Reservation made successfully!", "alert-success")
+        return redirect("main:home_view")
+
+    return render(request, "reservations/make_reservation.html")
