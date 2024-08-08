3dc42eccb5f19ac36ac4c232ba187eeb540d8406
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-08-08 08:40:08 +0300

    Implement Doctor Management

--- HospitalSystem/doctor/urls.py
+++ HospitalSystem/doctor/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = "doctor"
-urlpatterns = []
+
+urlpatterns = [
+    path("allDoctors/", views.all_doctors_view, name="all_doctors_view"),
+    path("addDoctors/", views.add_doctors_view, name="add_doctors_view"),
+]
