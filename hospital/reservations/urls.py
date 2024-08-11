49178ef9c2b308a529d76e26ec46f1a9fe705006
Abdullah Aldhamen <a.h.aldhamen@hotmail.com>
2024-08-11 04:14:12 +0300

    extra edits

--- hospital/reservations/urls.py
+++ hospital/reservations/urls.py
@@ -6,7 +6,6 @@ app_name = "reservations"
 urlpatterns = [
     path("add/", views.add_reservation, name="add_reservation"),
     path("", views.reservation_list, name="reservation_list"),
-    path("create/", views.add_reservation, name="add_reservation"),
     path("update/<int:pk>/", views.reservation_update, name="reservation_update"),
     path("delete/<int:pk>/", views.reservation_delete, name="reservation_delete"),
 ]
